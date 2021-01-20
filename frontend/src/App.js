d77de94a5272a76cdd083d85a2f1ef176544a9cf
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2021-01-20 07:10:31 -0700

    commit before pushing to github

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './pages/HomeScreen'
+import ProductScreen from './pages/ProductScreen'
+import CartScreen from './pages/CartScreen'
+import LoginScreen from './pages/LoginScreen'
+import RegisterScreen from './pages/RegisterScreen'
+import ProfileScreen from './pages/ProfileScreen'
+import ShippingScreen from './pages/ShippingScreen'
+import PaymentScreen from './pages/PaymentScreen'
+import PlaceOrderScreen from './pages/PlaceOrderScreen'
 
 const App = () => {
   return (
-    <>
-      <h1>Welcom to ProShop</h1>
-    </>
+    <Router>
+      <Header />
+      <main>
+        <Container>
+          <Route path='/shipping' component={ShippingScreen} />
+          <Route path='/payment' component={PaymentScreen} />
+          <Route path='/placeorder' component={PlaceOrderScreen} />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
+          <Route path='/product/:id' component={ProductScreen} exact />
+          <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/' component={HomeScreen} exact />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
